aa22e6aa4f3c554b29598942f0bcb99d75fb40fb
AngusHo53 <yuhoyuho987@gmail.com>
2020-05-27 12:34:58 +0800

    Create apollo-sysytem-sample. Remove apollo-system.

---
+++ apollo-system-sample/src/components/Login.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-container fill-height justify-center align-center>
+    <!-- <v-layout row > -->
+    <v-flex xs12 sm6 md5 lg4>
+      <v-card class="mt-0 pt-0"   elevation="2" >
+        <v-card-title class="blue darken-1">
+          <h4 style="color:white">Apollo System</h4>
+        </v-card-title>
+        <v-card-text>
+          <form @submit.prevent="login">
+            <v-layout row wrap>
+              <v-flex xs12 md4>
+                <v-subheader>使用者ID</v-subheader>
+              </v-flex>
+              <v-flex xs12 md8>
+                <v-text-field
+                  class="input-group--focused mr-2"
+                  name="email"
+                  v-model="email"
+                  label="email"
+                  value="Input text"
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-layout row wrap>
+              <v-flex xs12 md4>
+                <v-subheader>密碼</v-subheader>
+              </v-flex>
+              <v-flex xs12 md8>
+                <v-text-field
+                  class="input-group--focused mr-2"
+                  name="password"
+                  type="password"
+                  v-model="pass"
+                  label="password"
+                  value="Input text"
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-btn type="submit">登入</v-btn>
+            <v-snackbar
+              v-if="error"
+              :timeout="timeout"
+              :top="true"
+              :multi-line="mode === 'multi-line'"
+              :vertical="mode === 'vertical'"
+              v-model="error"
+            >
+              {{ text }}
+              <v-btn class="pink--text" text @click.native="error = false"
+                >Close</v-btn
+              >
+            </v-snackbar>
+          </form>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <!-- </v-layout> -->
+  </v-container>
+</template>
+<script lang="ts">
+import { Component } from "vue-property-decorator";
+import Vue from "vue";
+import { userModule } from "@/store/modules/user";
+
+
+@Component
+export default class Login extends Vue {
+  private email = "admin@test.com";
+  private pass = "password";
+  private error = false;
+  private text = "";
+
+  public gotoDashboard() {
+    this.$router.push("/");
+  }
+
+  public async login() {
+    await userModule.signIn({ username: this.email, password: this.pass });
+    this.$router.push("/");
+  }
+
+}
+</script>
---
+++ apollo-system-sample/src/components/Table.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+     <v-card-text>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-text>
+    <v-data-table
+      class="elevation-1"
+      :search="search"
+      :headers="headers"
+      :items="items"
+      :page.sync="pagination.page"
+      :items-per-page="pagination.rowsPerPage"
+      hide-default-footer
+    >
+      <template v-slot:item.actions="{ item }">
+        <v-btn fab class="teal mr-2" small dark @click.native="$emit('edit', item)">
+          <v-icon>
+            mdi-pencil
+          </v-icon>
+        </v-btn>
+        <v-btn fab class="cyan" small  @click.native="$emit('remove', item)">
+          <v-icon>
+            mdi-trash-can-outline
+          </v-icon>
+        </v-btn>
+      </template>
+      <template slot="no-data">
+        <span>
+          <p class="pt-2 blue--text subheading"><v-icon medium class="blue--text">mdi-info</v-icon>Sorry, nothing to display here :(</p>
+        </span>
+      </template>
+    </v-data-table>
+    <div class="text-xs-center pt-2" v-if="isNotEmpty">
+      <v-pagination v-model="pagination.page" :length="pagination.pages" :total-visible="5" circle></v-pagination>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import { Entity } from '@/types';
+import { Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Table extends Vue {
+  @Prop() readonly headers: TableHeader[];
+  @Prop() readonly items: Entity[];
+  @Prop() readonly pagination: Pagination;
+
+  search ='';
+
+  editItem() {}
+
+  deleteItem() {}
+
+  renderData = (item: TODO, header: TODO) => {
+    let val = '';
+    if (header.value.includes('.')) {
+      const vals = header.value.split('.');
+      val = vals.reduce((acc: TODO, val: TODO) => acc[val], item);
+    } else {
+      val = item[header.value];
+    }
+    if (typeof val === 'boolean') {
+      val = val ? 'Yes' : 'No';
+    }
+    return val;
+  };
+
+  isNotEmpty() {
+    return this.items && this.items.length > 0;
+  }
+
+  hasHeader(header: TODO) {
+    return header?.value ? true : false;
+  }
+}
+</script>
